a606ae58745a30238ebb7888ea045a928e85b658
Luke1024 <chajdas.lukasz@gmail.com>
2021-11-12 17:42:35 +0100

    url modified

--- main.js
+++ main.js
@@ -205,7 +205,7 @@ class MessageServiceService {
         this.pingingIntervalInSeconds = 15;
         this.storageKey = "local_token";
         this.token = "";
-        this.rootUrl = "http://localhost:8080/input";
+        this.rootUrl = "https://powerful-savannah-44442.herokuapp.com/input";
         this.tokenUrl = this.rootUrl + "/token";
         this.pulseUrl = this.rootUrl + "/load";
         this.operationsStorage = [];
